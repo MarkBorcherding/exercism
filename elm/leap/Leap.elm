53ccf8fe7a639098831c4a60cb5131044ee5b1e0
Mark Borcherding <markborcherding@gmail.com>
2018-02-28 18:18:06 -0600

    leap

--- elm/leap/Leap.elm
+++ elm/leap/Leap.elm
@@ -1,1 +1,10 @@
 module Leap exposing (..)
+
+every n y = (%) y n
+            |> (==) 0
+
+every4 = every 4
+every100 = every 100
+every400 = every 400
+
+isLeapYear y = every4 y && ( not(every100 y) || every400 y)
