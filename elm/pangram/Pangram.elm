9a371392f2ee2a476dbc05c3ea45822b3aad49cb
Mark Borcherding <markborcherding@gmail.com>
2018-02-28 21:18:31 -0600

    pangram

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -1,1 +1,12 @@
 module Pangram exposing (..)
+
+import Set
+import Regex
+
+isPangram s =
+    String.toLower s
+    |> Regex.find Regex.All (Regex.regex "[a-z]")
+    |> List.map .match
+    |> Set.fromList
+    |> Set.size
+    |> (==) 26
